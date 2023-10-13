866417ed8a75021e8f3cf43b21c92c7ce8375723
Pedro Vidal Villalba <pedrovidalvillalba@gmail.com>
2023-10-13 12:54:27 +0200

    PROCESADO DE ARGUMENTOS CLIENTE
    El cliente ya puede procesar los argumentos por línea de comandos.
    Además, se eliminaron los ejecutables del control de versiones (a
    machete, hay que cambiarlo).

--- basic/cliente.c
+++ basic/cliente.c
@@ -4,30 +4,33 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #define NUM_BYTES_RECV 120
 
+/**
+ * Process the command line inputs given to main
+ * @param argc  Number of arguments.
+ * @param argv  Array of strings with the program arguments.
+ * @return void
+ */
+void process_args(int argc, char** argv, char* server_ip, uint16_t* port);
+
 int main(int argc,char **argv){
 	Client client;
-	char server_ip[INET_ADDRSTRLEN]="127.0.0.1";
+	char server_ip[INET_ADDRSTRLEN];
+    uint16_t port;
 	
-	
-	client = create_client(AF_INET, SOCK_STREAM, 8000, server_ip);
+    process_args(argc, argv, server_ip, &port);
+	client = create_client(AF_INET, SOCK_STREAM, port, server_ip);
 	connect_to_server(client); 
 	handle_data(client);
 	
-	
-	
-	/*LAS PALABRAS DEL PROFESOR CABA RESUENAN EN TU CABEZA: CIERRA LOS SOCKETS!*/
 	close(client.socket);
-
 }
 
 Client create_client(int domain, int service, uint16_t port, char* server_ip){
 	Client client;
-	
-	
-	
 	
 	//Creamos el socket y comprobamos su correcta creación
 	if ((client.socket = socket(domain, service, 0)) < 0) {
@@ -37,7 +40,10 @@ Client create_client(int domain, int service, uint16_t port, char* server_ip){
 	
 	client.address.sin_family = domain,//AF_INET;
 	client.address.sin_port = htons(port);
-	inet_pton(domain, server_ip, &(client.address.sin_addr));//aqui podemos comprobar que se ha 	realizado la conversión con éxito 
+    if (!inet_pton(domain, server_ip, &(client.address.sin_addr))) {    /* La string no se pudo traducir a una IP válida */
+        fprintf(stderr, "La IP especificada no es válida\n\n");
+        exit(EXIT_FAILURE);
+    }; 
 	
 	return client;
 }
@@ -62,4 +68,50 @@ void handle_data(Client client){
 		
 		printf("Mensaje recibido: %s", server_message);
 
+}
+
+void print_help(void) {}
+
+void process_args(int argc, char** argv, char* server_ip, uint16_t* port) {
+    int i;
+    char* current_arg;
+
+    /* Si no se pasan argumentos, avisar y poner las opciones por defecto */
+    if (argc == 1) {
+        fprintf(stderr, "No se ha especificado la IP y el puerto al que conectarse\n\n");
+        print_help();
+        exit(EXIT_FAILURE);
+    }
+
+    for (i = 1; i < argc; i++) { /* Procesamos los argumentos (sin contar el nombre del ejecutable) */
+        current_arg = argv[i];
+        if (current_arg[0] == '-') { /* Flag de opción */
+            switch(current_arg[1]) {
+                case 'I':   /* IP */
+                    if (++i < argc) {
+                        strncpy(server_ip, argv[i], INET_ADDRSTRLEN);
+                    } else {
+                        fprintf(stderr, "IP no especificada tras la opción '-I'\n\n");
+                        print_help();
+                        exit(EXIT_FAILURE);
+                    }
+                    break;
+                case 'p':   /* Puerto */
+                    if (++i < argc) {
+                        *port = atoi(argv[i]);
+                    } else {
+                        fprintf(stderr, "Puerto no especificado tras la opción '-p'\n\n");
+                        print_help();
+                        exit(EXIT_FAILURE);
+                    }
+                    break;
+                case 'h':
+                    print_help();
+                    exit(EXIT_SUCCESS);
+                default:
+                    fprintf(stderr, "Opción '%s' desconocida\n\n", current_arg);
+                    exit(EXIT_FAILURE);
+            }
+        }
+    }
 }
